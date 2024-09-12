b7fbfcb2a59ba7feae1917e1e1ea1b4566aa05ac
ljxpython <1030470148@qq.com>
2024-09-12 18:57:51 +0800

    添加允许跨域请求

--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
 from plant_srv import create_app
-
+from conf.config import settings
 
 
 app = create_app()
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5001)
+    app.run(debug=True, host='0.0.0.0', port=settings.port)
